47bc4ef32253893fc755b9737cfcd2cebcf768c6
Tev <tevondavisjc@gmail.com>
2021-11-03 18:52:41 -0500

    import hooks

---
+++ hooks/index.js
@@ -0,0 +1,21 @@
+import useBackground from "./useBackground";
+import useRestResponse from "./useRestResponse";
+import { useWindowSize } from "./useWindowSize";
+import useOnClickOutside from "./useOnClickOutside";
+import useKeyPress from "./useKeyPress";
+import useNavbar from "./useNavbar";
+import useDialog from "./useDialog";
+// import useLockBodyScroll from "./useLockBodyScroll";
+// import useInput from "./useInput";
+
+export {
+  useNavbar,
+  useWindowSize,
+  useOnClickOutside,
+  useBackground,
+  useRestResponse,
+  useKeyPress,
+  useDialog,
+  //   useLockBodyScroll,
+  //   useInput,
+};

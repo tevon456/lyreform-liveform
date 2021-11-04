db8fd0130694d9c0ca0a5bc292340344c1ecae25
Tev <tevondavisjc@gmail.com>
2021-11-03 22:01:55 -0500

    created form header

---
+++ components/dynamic_form/FormHeader.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from "react";
+import Image from "next/image";
+import styled from "styled-components/macro";
+import { UICore } from "..";
+
+const StyledFormHeader = styled.div`
+  border-radius: 4px 4px 0px 0px;
+  padding: 1em;
+  z-index: 2;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+/**
+ * Form header
+ * @prop {string} name - form name.
+ * @prop {string} headerBackground - header background color.
+ * @prop {string} headerColor - header text color.
+ * @prop {string} logo - form logo url.
+ * @prop {string} logoWidth - logo pixel width.
+ * @prop {string} logoHeight - logo pixel height.
+ */
+function FormHeader({
+  name,
+  logo_url,
+  header_foreground,
+  header_background,
+  logo_width = "64px",
+  logo_height = "64px",
+}) {
+  return (
+    <StyledFormHeader
+      css={`
+        background: ${header_background};
+      `}
+    >
+      {logo_url ? (
+        <Image
+          src={logo_url}
+          width={logo_width}
+          height={logo_height}
+          alt="organization's logo"
+        />
+      ) : null}
+      <UICore.Text as="h3" size="lg" weight={700} color={header_foreground}>
+        {name}
+      </UICore.Text>
+    </StyledFormHeader>
+  );
+}
+export default FormHeader;

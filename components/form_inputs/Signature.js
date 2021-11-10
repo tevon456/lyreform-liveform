74ef4e2a2a9b4d4080878dcdb884e33039ba1559
Tev <tevondavisjc@gmail.com>
2021-11-10 15:32:28 -0500

    fixed warnings

--- components/form_inputs/Signature.js
+++ components/form_inputs/Signature.js
@@ -32,15 +32,15 @@ function Signature(props) {
           Click to here sign{" "}
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="icon icon-tabler icon-tabler-pencil"
+            className="icon icon-tabler icon-tabler-pencil"
             width="28"
             height="28"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke={props.checkColor}
             fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           >
             <path stroke="none" d="M0 0h24v24H0z" />
             <path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4" />

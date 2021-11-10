777706bb7364cb21e1fc6449518565781d215589
Tev <tevondavisjc@gmail.com>
2021-11-10 17:03:20 -0500

    testing origins

--- pages/preview.js
+++ pages/preview.js
@@ -10,7 +10,7 @@ export default function Preview() {
     window?.addEventListener(
       "message",
       (event) => {
-        if (allowedOrigins.includes(even.origin)) {
+        if (!allowedOrigins.includes(even.origin)) {
           console.log("no");
           return;
         }

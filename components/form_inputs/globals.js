f668b54a6b9193ba3683f4f8ade7bfee8934a9e9
Tev <tevondavisjc@gmail.com>
2021-11-10 15:39:52 -0500

    switched input borders

--- components/form_inputs/globals.js
+++ components/form_inputs/globals.js
@@ -1,6 +1,6 @@
 export const globals = {
   borderRadius: "4px",
-  border: "0.15em solid black",
+  border: "0.15em solid #141414a3",
   shadow:
     "rgba(108, 108, 108, 0.12) 0px 2px 4px 0px,inset rgba(108, 108, 108, 0.26) 0px 3px 2px 0px",
   shadowHover: "#fff 0px 0px 0px 2px, #111 0px 0px 0px 3px",

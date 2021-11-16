67d36979b29eea840cb68cac682650fc555bc619
Tev <tevondavisjc@gmail.com>
2021-11-16 00:23:47 -0500

    add logo to loader

--- components/ui-core/Loader.js
+++ components/ui-core/Loader.js
@@ -1,4 +1,6 @@
 import styled, { keyframes } from "styled-components";
+import { UICore } from "..";
+import Logo from "../../icons/logo";
 
 const Frames = keyframes`
   0% { left:0px; top:0px;}
@@ -11,7 +13,8 @@ const Outer = styled.div`
   width: 100px;
   height: 8px;
   padding: 2px;
-  border: 2px solid #111;
+  margin-top: 4px;
+  border: 2px solid #2c2c34;
   border-radius: 12px;
 `;
 
@@ -19,18 +22,21 @@ const Inner = styled.div`
   width: 30px;
   height: 8px;
   border-radius: 12px;
-  background-color: #111;
+  background-color: #2c2c34;
   position: relative;
   animation-name: ${Frames};
-  animation-timing-function: ease-in-out;
-  animation-duration: 3s;
+  animation-timing-function: cubic-bezier(0.58, 0.33, 0.44, 0.7);
+  animation-duration: 2s;
   animation-iteration-count: infinite;
 `;
 
 export default function Loader() {
   return (
-    <Outer>
-      <Inner />
-    </Outer>
+    <UICore.Flex justify="center" align="center" direction="column">
+      <Logo width="28px" height="28px" colorA="#2c2c34" colorB="#2c2c34" />
+      <Outer>
+        <Inner />
+      </Outer>
+    </UICore.Flex>
   );
 }

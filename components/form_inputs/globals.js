47d98b23dbb7c9787d5ee6c3fe3af07a22ba43ea
Tev <tevondavisjc@gmail.com>
2022-02-04 22:52:22 -0500

    new inset and background color

--- components/form_inputs/globals.js
+++ components/form_inputs/globals.js
@@ -2,6 +2,7 @@ export const globals = {
   borderRadius: "4px",
   border: "0.15em solid #141414a3",
   shadow:
-    "rgba(108, 108, 108, 0.12) 0px 2px 4px 0px,inset rgba(108, 108, 108, 0.26) 0px 3px 2px 0px",
+    "rgba(108, 108, 108, 0.12) 0px 2px 4px 0px, rgba(167, 167, 167, 0.26) 0px 2px 1px 0px inset;",
   shadowHover: "#fff 0px 0px 0px 2px, #111 0px 0px 0px 3px",
+  backgroundColor: "#f5f5f5",
 };
